8bba037e07660041f918a01ae7a82600510490a6
primesh <primeshheshan@gmail.com>
2021-01-19 21:44:21 +0530

    recipe-list completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,16 @@ export class RecipeListComponent implements OnInit {
 recipes: RecipeModel[] = [
   new RecipeModel('Pizza',
                   'This is pizza recipe',
-                  'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg'),
+                  'https://bit.ly/2XWJGV3'),
+
+  new RecipeModel('burger',
+                  'Ground beef, or minced cow meat mince or other meat used to make hamburgers',
+                  'https://bit.ly/3iu86yA'),
+
+  new RecipeModel(' Fried Rice',
+                  'Fried rice is a dish of cooked rice that has been stir-fried in a wok',
+                  'https://bit.ly/38Xe0oO'),
+
 ];
   constructor() { }
 
