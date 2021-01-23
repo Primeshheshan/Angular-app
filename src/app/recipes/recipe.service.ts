e3ef9f9840c43bf8ffae2879f30b70f92fa3d03f
primesh <primeshheshan@gmail.com>
2021-01-23 15:52:49 +0530

    add recipe objects to recipe services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from "@angular/core";
+import { RecipeModel } from "./recipe.model";
 
 @Injectable()
-export class RecipeService {
 
+export class RecipeService {
+    private recipes: RecipeModel[] = [
+    new RecipeModel('Pizza',
+                    'This is pizza recipe',
+                    'https://bit.ly/2LEbzyZ'),
+
+    new RecipeModel('burger',
+                    'Ground beef, or minced cow meat mince or other meat used to make hamburgers',
+                    'https://bit.ly/3iu86yA'),
+
+    new RecipeModel(' Fried Rice',
+                    'Fried rice is a dish of cooked rice that has been stir-fried in a wok',
+                    'https://bit.ly/38Xe0oO'),
+
+  ];
+
+  getRecipe() {
+    return this.recipes.slice(); //we can get a copy of array
+  }
 }
