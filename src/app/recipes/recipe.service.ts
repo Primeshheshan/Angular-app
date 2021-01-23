04a7dda323040e04db03d58d2a33505a89baf4f6
primesh <primeshheshan@gmail.com>
2021-01-23 19:26:54 +0530

    particular ingredients added to the recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { IngredientModel } from "../shared/ingredients.model";
 import { RecipeModel } from "./recipe.model";
 
 @Injectable()
@@ -8,15 +9,25 @@ export class RecipeService {
   private recipes: RecipeModel[] = [
     new RecipeModel('Pizza',
                     'This is pizza recipe',
-                    'https://bit.ly/2LEbzyZ'),
+                    'https://bit.ly/2LEbzyZ',
+                    [
+                      new IngredientModel('cheese', 2),
+                      new IngredientModel('sausages', 2),
+                    ]),
 
     new RecipeModel('burger',
                     'Ground beef, or minced cow meat mince or other meat used to make hamburgers',
-                    'https://bit.ly/3iu86yA'),
+                    'https://bit.ly/3iu86yA',
+                    [
+                      new IngredientModel('buns', 2)
+                    ]),
 
     new RecipeModel(' Fried Rice',
                     'Fried rice is a dish of cooked rice that has been stir-fried in a wok',
-                    'https://bit.ly/38Xe0oO'),
+                    'https://bit.ly/38Xe0oO',
+                    [
+                      new IngredientModel('rice', 1)
+                    ]),
 
   ];
 
