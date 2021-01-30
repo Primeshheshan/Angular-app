cad744dae1d7ea072a938423584d7d5c23387323
primesh <primeshheshan@gmail.com>
2021-01-30 12:17:19 +0530

    changed path for redirect to recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,14 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/auth', pathMatch: 'full'},
-  { path: 'auth', component: AuthComponent},
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent ,
     children: [
       {path: '', component: RecipeStartComponent},
       {path: ':id', component: RecipeDetailComponent}
     ]},
   { path: 'shopping-list', component: ShoppingListComponent },
-
+  { path: 'auth', component: AuthComponent},
 ];
 
 @NgModule({
