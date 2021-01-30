fda4549d45295e1a9f81fd11d3ae1d9c76ff3f72
primesh <primeshheshan@gmail.com>
2021-01-30 09:47:45 +0530

    added authentication and logout tabs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   public isCollapsed = true;
   public isCollapsedDropdown = true;
+  private userSub!: Subscription;
+  isAuthenticated = false;
 
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      // this.isAuthenticated = !user ? false : true;
+      this.isAuthenticated = !!user; // if haven't user then false else true
+      console.log(!user);
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
